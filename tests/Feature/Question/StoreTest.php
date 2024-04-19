440cebf27f446616a6fdcd0fd09771f9f89d9190
dandevweb <danilo.pacheco@soitic.com>
2024-04-19 08:07:50 -0300

    API-3: when creating a new question, the status should be draft

--- tests/Feature/Question/StoreTest.php
+++ tests/Feature/Question/StoreTest.php
@@ -18,3 +18,19 @@ it('should be able to store a new question', function () {
         'question' => 'What is the capital of France?',
     ]);
 });
+
+test('after creating a new question, I need to make sure that it creates on draft_status', function () {
+    $user = \App\Models\User::factory()->create();
+
+    Sanctum::actingAs($user);
+
+    postJson(route('questions.store'), [
+        'question' => 'What is the capital of France?',
+    ])->assertSuccessful();
+
+    assertDatabaseHas('questions', [
+        'user_id'  => $user->id,
+        'status'  => 'draft',
+        'question' => 'What is the capital of France?',
+    ]);
+});
