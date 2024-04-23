e9dc3af0b65c1a6d781fb083da4c51af9dfbbd47
dandevweb <danilo.pacheco@soitic.com>
2024-04-23 06:21:29 -0300

    API-6: it should be able to archive a question

---
+++ tests/Feature/Question/ArchiveTest.php
@@ -0,0 +1,65 @@
+<?php
+
+use Laravel\Sanctum\Sanctum;
+
+use App\Models\{Question, User};
+use function Pest\Laravel\{ assertSoftDeleted,  patchJson};
+
+it('should be able to archive a question', function () {
+    $user     = User::factory()->create();
+    $question = Question::factory()
+        ->for($user)
+        ->create();
+
+    Sanctum::actingAs($user);
+
+    patchJson(route('questions.archive', $question))
+        ->assertNoContent();
+
+    assertSoftDeleted('questions', [
+        'id' => $question->id,
+    ]);
+
+    expect($question)->refresh()->deleted_at->not->toBeNull();
+});
+
+// it('should make sure that only person who created the question can archive it', function () {
+//     $rightUser = User::factory()->create();
+//     $wrongUser = User::factory()->create();
+
+//     $question = Question::factory()->create([
+//         'draft'      => true,
+//         'created_by' => $rightUser->id,
+//     ]);
+
+//     Sanctum::actingAs($wrongUser);
+
+//     patchJson(route('questions.archive', $question->id))
+//         ->assertForbidden();
+
+//     Sanctum::actingAs($rightUser);
+
+//     patchJson(route('questions.archive', $question->id))
+//         ->assertRedirect();
+// });
+
+// it('should be able  to restore an archived question', function () {
+//     $user     = User::factory()->create();
+//     $question = Question::factory()
+//         ->for($user, 'createdBy')
+//         ->create([
+//             'draft'      => true,
+//             'deleted_at' => now(),
+//         ]);
+
+//     Sanctum::actingAs($user);
+
+//     patchJson(route('question.restore', $question))
+//         ->assertRedirect();
+
+//     assertNotSoftDeleted('questions', [
+//         'id' => $question->id,
+//     ]);
+
+//     expect($question)->refresh()->deleted_at->toBeNull();
+// });
