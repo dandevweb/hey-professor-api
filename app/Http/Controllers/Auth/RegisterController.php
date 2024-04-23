bcdd4e1521707296e8179cc047dbf052903246d6
dandevweb <danilo.pacheco@soitic.com>
2024-04-23 07:29:36 -0300

    API-9: user register validation

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -3,20 +3,20 @@
 namespace App\Http\Controllers\Auth;
 
 use App\Models\User;
-use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Auth;
 
 class RegisterController extends Controller
 {
-    public function __invoke(Request $request)
+    public function __invoke()
     {
-        $user = User::create([
-            'name'     => $request->name,
-            'email'    => $request->email,
-            'password' => $request->password,
+        $data = request()->validate([
+            'name'     => 'required|min:3|max:255',
+            'email'    => 'required|min:3|max:255|email|unique:users',
+            'password' => 'required|min:8|max:40|confirmed',
         ]);
 
+        $user = User::create($data);
 
         Auth::login($user);
 
--- tests/Feature/Auth/RegistrationTest.php
+++ tests/Feature/Auth/RegistrationTest.php
@@ -20,3 +20,40 @@ it('should be able to register in the application', function () {
     expect(auth()->user()->name)->toBe('Test User');
 
 });
+
+describe('validations', function () {
+    test('name', function ($rule, $value, $meta = []) {
+        postJson(route('register'), ['name' => $value])
+            ->assertJsonValidationErrors([
+                'name' => __('validation.'.$rule, array_merge(['attribute' => 'name'], $meta)),
+            ]);
+    })->with([
+        'required' => ['required', ''],
+        'min:3'    => ['min', 'a', ['min' => 3]],
+        'max:255'  => ['max', str_repeat('a', 256), ['max' => 255]],
+    ]);
+
+    test('email', function ($rule, $value, $meta = []) {
+        postJson(route('register'), ['email' => $value])
+            ->assertJsonValidationErrors([
+                'email' => __('validation.'.$rule, array_merge(['attribute' => 'email'], $meta)),
+            ]);
+    })->with([
+        'required' => ['required', ''],
+        'min:3'    => ['min', 'a', ['min' => 3]],
+        'max:255'  => ['max', str_repeat('a', 256), ['max' => 255]],
+        'email'    => ['email', 'invalid-email'],
+    ]);
+
+    test('password', function ($rule, $value, $meta = []) {
+        postJson(route('register'), ['password' => $value])
+            ->assertJsonValidationErrors([
+                'password' => __('validation.'.$rule, array_merge(['attribute' => 'password'], $meta)),
+            ]);
+    })->with([
+        'required'  => ['required', ''],
+        'min:8'     => ['min', 'a', ['min' => 8]],
+        'max:40'    => ['max', str_repeat('a', 256), ['max' => 40]],
+        'confirmed' => ['confirmed', 'password', ['other' => 'password_confirmation']],
+    ]);
+});
