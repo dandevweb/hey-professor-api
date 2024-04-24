9a66e3c44966673fdb9b126beafc1ca947934643
dandevweb <danilo.pacheco@soitic.com>
2024-04-24 06:52:13 -0300

    API-10: testing login controller

--- tests/Feature/Auth/LoginTest.php
+++ tests/Feature/Auth/LoginTest.php
@@ -14,3 +14,27 @@ it('should be able to login', function () {
 
     assertAuthenticatedAs($user);
 });
+
+it('should check if the email and password is valid', function ($email, $password) {
+    User::factory()->create(['email' => 'joe@doe.com']);
+
+    postJson(route('login'), [
+        'email'    => $email,
+        'password' => $password,
+    ])->assertJsonMissingValidationErrors([
+        'email' => __('auth.failed'),
+    ]);
+
+})->with([
+    'wrong mail'     => ['wrong@email.com', 'password'],
+    'wrong password' => ['wrong@email.com', 'password12312'],
+    'invalid mail'   => ['invalid', 'password12313'],
+]);
+
+test('required fields', function () {
+    postJson(route('login'))
+        ->assertJsonValidationErrors([
+            'email'    => __('validation.required', ['attribute' => 'email']),
+            'password' => __('validation.required', ['attribute' => 'password']),
+        ]);
+});
